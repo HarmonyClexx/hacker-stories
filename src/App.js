a01260422ecee547cf61d67cfce82c824767e627
HarmonyClexx <harmonymupindu@gmail.com>
2024-11-13 22:27:33 +0200

    React State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import * as React from "react";
 
-const Item = (props)=>(
-  <li key={props.item.objectID}>
-          <span>
-            <a href={props.item.url}>{props.item.title} </a>
-          </span>
-          <span>{props.item.author} </span>
-          <span>{props.item.num_comments} </span>
-          <span>{props.item.points}</span>
-        </li>
-);
-const List = (props) => (
-  <>
-    <ul>
-      {props.list.map((item) => (
-        <Item key={item.objectID} item={item}/>
-      ))}
-    </ul>
-  </>
-);
-
 const App = () => {
   const stories = [
     {
@@ -39,26 +19,53 @@ const App = () => {
       objectID: 1,
     },
   ];
-  return(
-  <div>
-    <Search />
-    <hr />
-    {/* render the list here */}
-    <List list = {stories}/>
-  </div>
-)};
 
-const Search = ()=>{
-  const handleChange = (event)=>{
-    console.log(event.target.value);
-  }
-  return(
-    <>
-    <h1>My Hacker Stories</h1>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={handleChange}/>
-    </>
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
   );
-}
+};
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
 export default App;
